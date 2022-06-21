a7915426cc90f7c3dab41e7e2c3c9dc430e640a0
Furkan Selçuk <64227907+furkanselcuk11@users.noreply.github.com>
2022-06-21 12:00:13 +0300

    +add: Player Controller added

---
+++ Assets/Scripts/MobileInput.cs
@@ -0,0 +1,136 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobileInput : MonoBehaviour
+{
+    public static MobileInput instance;   // Diger Script'ler uzrerinden erisimi saglar
+
+    // Mouse Positions
+    private Vector2 start_pos;
+    Vector2 last_pos;
+    Vector2 delta;
+
+    [Header("Controllers")]
+    public bool tap;
+    public bool swipeLeft;
+    public bool swipeRight;
+    public bool swipeUp;
+    public bool swipeDown;
+    public bool swipe;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != null)
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    private void Start()
+    {
+        // Butýn boollari sifirliyoruz
+        tap = swipe = false;
+        swipeLeft = false;  // Sola kaydirma
+        swipeRight = false; // Saga kaydirma
+    }
+    private void FixedUpdate()
+    {
+        SwipeMove();
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {   // Mosue tusuna basildiginda veya ekranda parmak ile basildigýndaki ilk pozisyon degerini al?ir
+            start_pos = Input.mousePosition;    // ilk posizsyon de?geri tutulur
+            GameManager.gamemanagerInstance.startTheGame = tap = true; // Dokunma aktif olur  
+            // Oyuna baþlamak için ekrana dokun (StartGame) True olur
+        }
+
+        if (Input.GetMouseButton(0))
+        {   // Mosue tusuna basili tutuldugunda veya ekranda parmak ile basili tutularak gidildigindeki son pozisyonun degerini al?r
+            last_pos = Input.mousePosition; // Son pozisyon de?eri tutulur
+            delta = start_pos - last_pos;   // Toplam kaydirilan mesafe hesaplanir ve delta degerinde tutulur
+            swipe = true;   // Kaydirma aktif olur
+
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {   // Mosue tusuna basma birakildiginda veya ekranda parmak basma birakildiginda
+            if (start_pos == last_pos) swipe = false;
+            // Eger dokunulan ilk pozisyon ile son pozisyon degeri ayni ise kaydirma pasif olur
+            start_pos = Vector2.zero;
+            last_pos = Vector2.zero;
+            delta = Vector2.zero;
+            // Tum degerler sifirlanir tekrar dokunma islemine kadar
+
+            swipeRight = false;
+            swipeLeft = false;
+            tap = false;
+            // Tum bool degerler sifirlanir tekrar dokunma islemine kadar
+        }
+    }
+    void SwipeMove()
+    {
+        #region Mobile Controller 4 Direction
+        if (tap)    // Eger dokunma islemi aktif ise calisir
+        {
+            if (swipe)  // Eger swipe(kaydirma) islemi aktif ise calisir
+            {
+                if (delta.magnitude > 100)  // delta degerinin uzunluk bilgisini alir ve 100 degerinden buyukse calisir - 100 degeri minimum kaydirma mesafesi
+                {
+                    float x = delta.x;  // Kaydirma mesafesinin x degerini alir
+                    float y = delta.y;  // Kaydirma mesafesinin y degerini alir
+                    if (Mathf.Abs(x) > Mathf.Abs(y))    // Eger kaydirma mesafesinin x ekseni y ekseninden daha buyukse (Right-Left) degilse (Up-Down) kaydirma aktif olur
+                    {
+                        // Right-Left
+                        if (x < 0)
+                        {
+                            // Saga kaydirma aktif olur 
+                            swipeRight = true;
+                            swipeLeft = false;
+                            swipeUp = false;
+                            swipeDown = false;
+                        }
+                        else
+                        {
+                            // Sola kaydirma aktif olur 
+                            swipeRight = false;
+                            swipeLeft = true;
+                            swipeUp = false;
+                            swipeDown = false;
+                        }
+                    }
+                    else
+                    {
+                        // Up-Down
+                        if (y < 0)
+                        {
+                            // ileri kaydirma aktif olur 
+                            swipeRight = false;
+                            swipeLeft = false;
+                            swipeUp = true;
+                            swipeDown = false;
+                        }
+                        else
+                        {
+                            // Geri kaydirma aktif olur 
+                            swipeRight = false;
+                            swipeLeft = false;
+                            swipeUp = false;
+                            swipeDown = true;
+                        }
+                    }
+                }
+            }
+            else
+            {   // Eger kaydirma islemi pasif ise 
+                tap = false;    // Dokunma pasif olur
+            }
+        }
+        #endregion
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rb;
+
+    [Header("Player Controller")]
+    [SerializeField] private float speed = 30f;    // Player hareket hýzý
+    [SerializeField] private float horizontalspeed = 10f; // Player yön hareket hýzý
+    [SerializeField] private float defaultSwipe = 4f;    // Player default kaydýrma mesafesi
+
+
+    private Animator anim;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+    }
+    private void FixedUpdate()
+    {
+        if (GameManager.gamemanagerInstance.startTheGame & !GameManager.gamemanagerInstance.isFinish)
+        {
+            // Eðer StartGame true ve isFinish false ise hareket et
+            transform.Translate(0, 0, speed * Time.fixedDeltaTime); // Karakter speed deðeri hýzýdna ileri hareket eder
+            anim.SetBool("isRunning", true);    // Koþma animasyonu çalýþýr
+            MoveInput();    // Player hareket kontrolü çalýþtýr
+        }
+        else
+        {
+            // Eðer StartGame False ise  hareket etmez
+            anim.SetBool("isRunning", false);   // Koþma animasyonu durur ve default olarak bekleme animsayonu çalýþýr
+        }
+    }
+    void MoveInput()
+    {
+        #region Mobile Controller 4 Direction
+
+        float moveX = transform.position.x; // Player objesinin x pozisyonun de?erini al?r      
+        float moveZ = transform.position.z; // Player objesinin z pozisyonun de?erini al?r           
+
+        if (Input.GetKey(KeyCode.LeftArrow) || MobileInput.instance.swipeLeft)
+        {   // Eðer klavyede sol ok tuþuna basýldýysa yada "MobileInput" scriptinin swipeLeft deðeri True ise  Sola hareket gider
+            moveX = Mathf.Clamp(moveX - 1 * horizontalspeed * Time.fixedDeltaTime, -defaultSwipe, defaultSwipe);    // Pozisyon sýnýrlandýrýlmasý koyulacaksa
+            // Player objesinin x (sol) pozisyonundaki gideceði min-max sýnýrý belirler
+        }
+        else if (Input.GetKey(KeyCode.RightArrow) || MobileInput.instance.swipeRight)
+        {   // Eðer klavyede sað ok tuþuna basýldýysa yada "MobileInput" scriptinin swipeRight deðeri True ise Saða hareket gider   
+            moveX = Mathf.Clamp(moveX + 1 * horizontalspeed * Time.fixedDeltaTime, -defaultSwipe, defaultSwipe);    // Pozisyon sýnýrlandýrýlmasý koyulacaksa
+            // Player objesinin x (sað) pozisyonundaki gideceði min-max sýnýrý belirler
+        }
+        else
+        {
+            rb.velocity = Vector3.zero; // E?er hareket edilmediyse Player objesi sabit kals?n
+        }
+
+        transform.position = new Vector3(moveX, transform.position.y, moveZ);
+        // Player objesinin pozisyonu moveX deðerine göre x ekseninde, moveZ deðerine göre z ekseninde hareket eder ve y ekseninde sabit kalýr  
+
+        #endregion
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Fruit"))
+        {
+            GameManager.gamemanagerInstance.Add(other.gameObject);
+        }
+        if (other.CompareTag("Fail"))
+        {
+            GameManager.gamemanagerInstance.Restart();
+        }
+        if (other.CompareTag("Finish"))
+        {
+            // Finish alanýna geldiyse Level biter ve hareket etme false durumuna geçer
+            GameManager.gamemanagerInstance.isFinish = true;
+            GameManager.gamemanagerInstance.startTheGame = false;
+            // Dans animasyonu
+            // Konfeti patlat            
+        }
+    }
+}
